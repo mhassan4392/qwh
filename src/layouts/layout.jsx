86e3ecf543a4ff8eb4c068606abbc5cd260c62f0
Muhammad Hassan <mhassan4392@gmail.com>
2022-05-16 11:20:01 +0500

    fix padding bottom bug

--- src/layouts/layout.jsx
+++ src/layouts/layout.jsx
@@ -7,8 +7,8 @@ const Layout = () => {
   const [paddingBottom, setPaddingBottom] = useState(0);
   return (
     <div
-      className="h-full relative bg-light bg-opacity-25"
-      style={{ paddingBottom: paddingBottom + "px" }}
+      className="h-full relative bg-light bg-opacity-25 overflow-y-auto"
+      style={{ paddingBottom: "100px" }}
     >
       <header className="bg-white">
         <Navbar />
