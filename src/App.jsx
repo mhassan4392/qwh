8008113b34cab315423adba70a3ed7eb2df830c4
Muhammad Hassan <mhassan4392@gmail.com>
2022-05-29 22:41:53 +0500

    add image blob to /entry/register

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ import authenticate from "@/store/features/auth/authenticate";
 const App = () => {
   // const dispatch = useDispatch();
   // const user = JSON.parse(localStorage.getItem("user"));
-  useEffect(() => {
-    Axios({ url: "/page/load", method: "POST" }).then((res) =>
-      console.log(res)
-    );
-  }, []);
+  // useEffect(() => {
+  //   Axios({ url: "/page/load", method: "POST" }).then((res) =>
+  //     console.log(res)
+  //   );
+  // }, []);
   return (
     <div className="h-screen overflow-x-hidden">
       <AllRoutes />
--- src/pages/entry/register.jsx
+++ src/pages/entry/register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import login_icon from "@/assets/images/entry/login_icon.webp";
 import { BiUserCircle } from "react-icons/bi";
 import {
@@ -11,16 +11,29 @@ import { IoLanguage } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+import Axios from "@/utils/axios";
+
 import { useSelector, useDispatch } from "react-redux";
 
 import { useForm } from "react-hook-form";
 
 const Register = () => {
+  useEffect(() => {
+    Axios({
+      url: "/SignUp/validCode",
+      method: "POST",
+      responseType: "blob",
+    }).then((res) => {
+      const url = URL.createObjectURL(res.data);
+      setCodeImage(url);
+    });
+  }, []);
   const { loading, error } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [codeImage, setCodeImage] = useState(false);
 
   const {
     register,
@@ -37,6 +50,19 @@ const Register = () => {
     console.log(data);
     console.log(isValid);
   });
+
+  const checkfile = (e) => {
+    console.log(e.target.files[0]);
+
+    const reader = new FileReader();
+
+    reader.onloadend = () => {
+      console.log(reader.result);
+      setCodeImage(reader.result);
+    };
+
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
   return (
     <motion.div initial={{ x: "100%" }} animate={{ x: 0 }} exit={{ x: "100%" }}>
@@ -146,13 +172,22 @@ const Register = () => {
                 <input
                   type="text"
                   placeholder="验证码"
-                  className="!bg-white !bg-opacity-30"
+                  className="!bg-white !bg-opacity-30 !pr-[125px]"
                   {...register("code", { required: "Code is required" })}
                 />
+                <span className="right-element">
+                  {codeImage && (
+                    <img
+                      src={codeImage}
+                      className="w-20 h-[38px] mr-1"
+                      alt=""
+                    />
+                  )}
+                </span>
                 {code && (
                   <MdOutlineCancel
                     onClick={() => setValue("code", "")}
-                    className="right-element"
+                    className="right-element !mr-[100px]"
                   />
                 )}
               </div>
@@ -184,3 +219,5 @@ const Register = () => {
 };
 
 export default Register;
+
+// data: '�PNG\r\n\u001a\n\u0000\u0000\u0000\rIHDR\u0000\u0000\u0000t\u0000\u0000\u0000$\b\u0006\u0000\u0000\u0000X\u0014\u001e�\u0000\u0000\u0000\u0004gAMA\u0000\u0000��\u000b�a\u0005\u0000\u0000\u0000\u0001sRGB\u0000��\u001c�\u0000\u0000\u0000 cHRM\u0000\u0000z&\u0000\u0000��\u0000\u0000�\u0000\u0000\u0000��\u0000\u0000u0\u0000\u0000�`\u0000\u0000:�\u0000\u0000\u0017p��Q<\u0000\u0000\t\u001eIDATx��yP��\u001d�?,�ܗ��6  (HU��H�\nb�\u0012���`:զvluL4�H\u001a�5�j�zLS�x���8�D�1Z\u001c���xD�\u0007 "7��,��E� ����ˮ�X>3;�\\��y����y~���RR���^^\u0018$�=�^�K��/\u0000���ҧ��8�^\u001c�PH\u001dҝ�ߑ��D�.\u0016Sp��{w���}��\u001a_?\u000f��\u0003\u0019\u0017\u001f�쟍�?��\u001b�݋)����6os��k\u000f�h�V������LK�q��z\u0011���\u00172��i�L�\u00173,,��q!�\u0014�\u0017OO\u001955O9�{�������ظᨠ��I���9��aT����<\f��!���/�_���!��*5\u001a-�-*\u001e��\u0003Ч����L\u00056���+\u000e1\u001ei�\u0014�(&������\u0013���C\n��P_/G�����Xpl͛{�\u0016ƚ�:\\�\u001c,��:Ǿ/�\u0001�\u001d\u000b\u0018=&ԦaéV^^�܅�m^c�����<�Bf\u0012�3���&��\u001bf\n^+j��b���x,JPS1���קw4�\u0017ӭ�\'�Ș����\u0005\u001bsD]o�C?X�5W.?\u0000`חo\u0013\u0019\u0015d�\u0000��%�.M�X\'��B��͖�0� �\u0011,\to�nO���Z+\u0017l��I�3��\t6<���\n�\u0016�\u0002\u0010\u0019\u0015d���\u0004D�2$m1\u0005[V\u0002�V��V��:Ə����H��M��Qt\u0017b�Ik\b\n�Ԥ`ӧ�hkk��Ņ�˧;�\u0011�{@ \u0003\u0013f0v�6\u0000�j5�&n�}\u0003M\u000f��:w��ѯ�\u0017\u0011\r�{�i��p�\u0015���8F|t\u0010\'�nۢ�j��u�\u000fQY�\u0000��_M$n��uS,\u0003\u0013f\u0018�\u0015u�4\u0014���˜Y�F@d,Cӗ\u00124~�Q;ӛ/$FO�^�j�,ٚ˓��fb:�:��P�F��5G8{�.\u0000�Ӈ��O^G"q\u00114`\r��3x�\u0014Ư�\u0014l�ll��p\u0016�\u0007�\u0002\u0010�<�Q�폌�Q�[��CeT_o��\u0002�R������~\f�\u0012L���x�uwj�:�j��_w�b�51�Y?��C�\u001a-\u001b�\u001dՋ9eZ4\u001f�N鴘���߇���q�P\u0016mZ-e9\u0007\u0018�l-.\u0012�\u001f\b��j���Q�[cV�Z������Ku\\�UH�ֱ�M\u001b�p�����,�4E��h�h�lX{�3�w\u0000xu�0>Y���\u0001Bg\u0018��C/�=��b��;�z1�|e�[6�ٙ��ٗ��O�\b��\u0003�≒�K.Q��)��0�bg��:վ!�5\u001f\u001f��\u0015Ν)\u0004`��X�~\u000e�n\u0010\u0013 xb"�s\u0016r�P\u0016\u000f��g���\u0004���i{�W\u001f��j\u0000�\u0007x��ѩx��0j3�p�m���=EhUZ��z��_��+\u000b!�\u0015\t�k\u0005�\t�SP�R���_��\u0017\u001a�9�5\u001b�OL\u001dC��\u0006�T\u0006@����\u0018*���鸅\u0011fb꘰2\u0006W���]�_�P���׺\t?�i���r�g�7�hQ���G�����s�i�h���D��d�u�rg�\u001e�\u000f��#h��\n��\nP66��ߧS��-\u001d7�/T���K�g?w�U��Z����\b\u001dە��\u0005��)(ק�}s��}\u0003��m瑶�f�0[/-�i\u000f�;"EG����w\u000b��|\u0005��U�̶9b�{���+/\u0003���S�,���Nժaw�14J-������v�%td\u0007\u001d�\u0015��� \u0012F\f2j+$b���]\u0011.���[��X�$�~;\u0000��jQF,q��"�m\u0007��z{��Gb˛������N�!"y ����=�����I\u000b�3�|[���6\u001ae�3ᑿ���\u000fko\u000fX�Q�ª\u0017�gs�1���[C�\u0000�m:n��\u001bo���e�@�(�\u0004��ц���k�G�Ԩ�w>�yKI�5��K.�T��?��e�!�\u001f��Ʋ\u0016�\u001c(��r�:;n�0�!�\u0000Ehj�EI�S�2C\u0011\u001dEz�l\u0006��7��@ȻL�홲\rۦ\u001eLF�h��U�͖.\u0011̀�}I?�ȍ�\u000f���������\f�\u001a̸?\feP|�M{�y�l�3\u00054E\np�l��\r���U��\n\u0006T��ׅ�l;\u0012�\nJs_�4W�T-D��\n�\u001e.C�l9�);�\b��+>\u0003��\u000b�\u0012��\f1�\u001d{�a�.{��P\f�ެ�q\r\u0016jgK`���\u0013��)ή\u0000 <i �\u0017G�?6\u0000��+M�-\u0014gW���\u001d�OVR�_GP��E[�����u��(]"��I��zCa:;U��3�\u001b<�\u0007\u0000mC\u0013\u0005�_5k?h�彩%z��:��\u0004���&�\u0017O�?B���\u0012�z\u0003�j\u0005\u0012�\n}y��d��[G�\\�Ȩ���kO�˦��zG�\u001e�j+�&Z�jyr�\u0006q�o���>�\u000e�\u0001:^�\b���čY��#��XL�V���T���|���>/��r��>,5V�.��Q�S�\u0012�(\u0015Fea)\u000b\u00188������\u001eM���\u0003�d\u000e=�V݆w�\u0007o_�e���\u001e��O���2\u000f5\u00147{�X�^��|��\r����I\u0000�#bh���\u0019i�.��ӏ�tB��x�DNs��ǅM��)bz�S��K ���\u001b��a���G���g�Y\u0007\n��E\u0007v\u00134f2~\u0011���皽��Y"^\u001bĕ��\u0000Hٖg�mzv����_Ơ�~N�;�\u0011�>���;}�Yb\u0002|XZ\r��\u0004��&EQ�]Iɩ*}YªX�\u001e�N\u001bCW�#\u0004u\u0006������쎙B"���ÉM\u000fw�غ\u0012Qo��\u0004&-=�\u000fRt�]�L\u0012-dVr\u0014E\'*yt�\u0011��g�Umx�J\t\b��\'/�g��������=���\t\t�y.�M�\u001fP�֎3ю\u0000\u0000\u0000\u0000IEND�B`�';
